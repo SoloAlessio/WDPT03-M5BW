67fb29d41709e998b707e15dec28b6dbf46ddbf9
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-11-14 16:08:16 +0000

    completato jumbotron inserita fetch get profilo personale

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,32 @@
 import { Container, Row, Col } from "react-bootstrap"
 import Jumbotron from "./Jumbotron/Index"
-export default function main() {
+import { useCallback, useEffect, useState } from "react"
+
+export default function Main() {
+
+    const [myProfile, setMyProfile] = useState()
+   
+    const getMyProfile = useCallback(() => {
+        fetch("https://striveschool-api.herokuapp.com/api/profile/me",{
+            headers: {
+                Authorization: "Bearer ...."
+                }
+        
+        })
+        .then((r) => r.json())
+        .then(setMyProfile)
+    }, [])
+
+    useEffect(()=> {
+        getMyProfile()
+    },[getMyProfile])
+
+
     return (
-        <Container>
+        <Container className="mt-4">
             <Row>
                 <Col xs={8}>
-                    <Jumbotron/>
+                    {myProfile && <Jumbotron myProfile={myProfile}/>}
                 </Col>
             </Row>
         </Container>
