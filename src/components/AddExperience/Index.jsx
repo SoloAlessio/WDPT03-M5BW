01bebb5e92b408dd6f67858379c3f3f59e6654ff
WillyWilliam00 <costawilliam98@gmail.com>
2023-11-15 08:12:53 +0100

    ip

--- src/components/AddExperience/Index.jsx
+++ src/components/AddExperience/Index.jsx
@@ -18,38 +18,44 @@ function Example() {
         Launch demo modal
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} className='ModalExp'>
         <Modal.Header closeButton>
           <Modal.Title>Aggiungi Esperienza</Modal.Title>
         </Modal.Header>
+        <div style={{background: "#EDF3F8", padding: "1rem"}}>
+            <h2 className='fs-7'>Informa la rete</h2>
+            <p className='m-0 AddExpLabel' style={{fontSize: 14}}>Attiva l’opzione per informare la tua rete delle principali modifiche al profilo 
+              (ad esempio un nuovo lavoro) e degli anniversari lavorativi. Gli aggiornamenti possono richiedere fino a 2 ore. 
+              Scopri di più sulla <span className='colorBlu fw-semibold'>condivisione delle modifiche del profilo</span>.</p>
+          </div>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Qualifica</Form.Label>
+            <Form.Group className="mb-3 AddExpLabel" controlId="formBasicEmail">
+              <Form.Label className='m-0 '>Qualifica</Form.Label>
               <Form.Control type="text" placeholder="Esempio: CTO" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Nome Azienda</Form.Label>
+            <Form.Group className="mb-3 AddExpLabel" controlId="formBasicEmail">
+              <Form.Label className='m-0 '>Nome Azienda</Form.Label>
               <Form.Control type="text" placeholder="Esempio: EPICODE" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Descrizione</Form.Label>
+            <Form.Group className="mb-3 AddExpLabel" controlId="formBasicEmail">
+              <Form.Label className='m-0 '>Descrizione</Form.Label>
               <Form.Control as="textarea"  />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Attualmente ricopro questo ruolo" onChange={handleChange} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Inizio:</Form.Label>
+            <Form.Group className="mb-3 AddExpLabel" controlId="formBasicEmail">
+              <Form.Label className='m-0 '>Inizio:</Form.Label>
               <input type="date" className='d-block'/>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Fine:</Form.Label>
+            <Form.Group className="mb-3 AddExpLabel" controlId="formBasicEmail">
+              <Form.Label className='m-0 '>Fine:</Form.Label>
               <input type="date" className='d-block' disabled={checked}/>
             </Form.Group>
             
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Località</Form.Label>
+            <Form.Group className="mb-3 AddExpLabel" controlId="formBasicEmail">
+              <Form.Label className='m-0 '>Località</Form.Label>
               <Form.Control type="text" placeholder="Esempio: Italia, Lombardia, Milano" />
             </Form.Group>
             
